bc3ca6d4d62d7b91e00d7e1239040fc3d8572c91
Aslam Sunkesula <aslam.sunkesula@gmail.com>
2025-03-17 23:44:58 +0530

    updated tfvars

--- development/infra.tf
+++ development/infra.tf
@@ -1,11 +1,12 @@
 module "dev_vpc_1" {
   source              = "../modules/network"
-  vpc_cidr            = "10.90.0.0/16"
-  vpc_name            = "dev_vpc_1"
-  environment         = "Development"
-  public_subnet_cidr  = ["10.90.1.0/24", "10.90.2.0/24", "10.90.3.0/24"]
-  private_subnet_cidr = ["10.90.10.0/24", "10.90.20.0/24", "10.90.30.0/24"]
-  azs                 = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  vpc_cidr            = var.vpc_cidr
+  vpc_name            = var.vpc_name
+  environment         = var.environment
+  public_subnet_cidr  = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+  azs                 = var.azs
+  aws_region          = var.aws_region
 }
 
 module "dev_natgw_1" {
--- production/variables.tf
+++ production/variables.tf
@@ -1,10 +1,11 @@
-# variable "vpc_name" {}
-# variable "vpc_cidr" {}
-# variable "environment" {}
-# variable "public_subnet_cidr" {}
-# variable "private_subnet_cidr" {}
-# variable "azs" {}
-# variable "aws_region" {}
+variable "vpc_name" {}
+variable "vpc_cidr" {}
+variable "environment" {}
+variable "public_subnet_cidr" {}
+variable "private_subnet_cidr" {}
+variable "azs" {}
+variable "aws_region" {}
+
 
 variable "aws_region" {
   default = "us-east-1"
